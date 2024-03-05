b20baa072214c579b0614dd80c58241bf676a352
Leonardo Luz <leonardoluz10@hotmail.com>
2024-03-05 20:59:55 -0300

    feat: :white_check_mark: Refatorando bugs e passando cor de fundo do time.

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -5,6 +5,7 @@ const ListaSuspensa = (props) => {
         <div className='lista-suspensa'>
             <label>{props.label}</label>
             <select value={props.valor} required={props.obrigatorio} onChange={value => props.aoAlterado(value.target.value)}>
+                <option value=''>Escolha o seu Time</option>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
--- src/componentes/Times/index.js
+++ src/componentes/Times/index.js
@@ -3,10 +3,10 @@ import './Time.css';
 
 const Time = (props) => {
     return (
-        (props.colaboradores.length > 0) ? <section className="time" key={props.nome} style={{ backgroundColor: props.corSecundaria }}>
+        (props.colaboradores.length > 0) ? <section className="time" style={{ backgroundColor: props.corSecundaria }}>
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
             <div className='colaboradores'>
-                {props.colaboradores.map(colaborador => <Colaborador key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)}
+                {props.colaboradores.map(colaborador => <Colaborador key={colaborador.nome} corDeFundo={props.corPrimaria} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)}
             </div>
         </section> : ''
     )
